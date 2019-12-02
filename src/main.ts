98a108a5feafd734ae3cef62989c226ec33e4945
Peter.Redmond <peter.redmond@checkout.com>
2019-12-02 15:34:33 +0000

    update action.yml

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import * as core from '@actions/core';
+import * as fs from 'fs';
 
 async function run() {
   try {
     const version = core.getInput('version');
-    console.log(`Version: ${version}`)
+
+    fs.readdirSync('./build').forEach(file => {
+      console.log(file);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   } catch (error) {
     core.setFailed(error.message);
   }
