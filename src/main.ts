196f5759fae0b1df9cac2a5d0262ef5f91c4a891
Peter.Redmond <peter.redmond@checkout.com>
2019-12-02 13:46:26 +0000

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    const version = core.getInput('version');
+    console.log(`Version: ${version}`)
   } catch (error) {
     core.setFailed(error.message);
   }
